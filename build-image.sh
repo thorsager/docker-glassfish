4d3b8df6f07440215d47b99b807a69e0521292ee
Kaspar Bach Pedersen <ext-kape@stofa.dk>
2019-10-10 13:15:49 +0200

    Add openj9 base

--- build-image.sh
+++ build-image.sh
@@ -8,6 +8,11 @@ docker build -f Dockerfile-alpine-base \
 	-t thorsager/glassfish-base:alpine3.7 \
 	.
 
+## build openj9 base image for gf
+docker build -f Dockerfile-openj9-base \
+	-t thorsager/glassfish-base:openj9 \
+	.
+
 docker build -f Dockerfile \
 	--build-arg from=thorsager/glassfish-base:alpine \
 	--build-arg gf_arch_url=${GF_ARCH_411} \
@@ -21,6 +26,11 @@ docker build -f Dockerfile \
 	-t thorsager/glassfish:glassfish4-alpine \
 	.
 
+docker build -f Dockerfile \
+	--build-arg gf_arch_url=${GF_ARCH_412} \
+	--build-arg from=thorsager/glassfish-base:openj9 \
+	-t thorsager/glassfish:glassfish4-openj9 \
+	.
 
 
 #docker build -f Dockerfile-jdk-alpine -t thorsager/glassfish:glassfish4-openjdk-8-alpine .
